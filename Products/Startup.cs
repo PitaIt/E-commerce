669743f705328fbf56a035514d0270980cbc1a71
Krystian Klik <KrystianKlik@gmail.com>
2021-10-22 18:32:51 +0200

    Modify Users and Products solutions

--- Products/Startup.cs
+++ Products/Startup.cs
@@ -12,7 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Users
+namespace Products
 {
     public class Startup
     {
@@ -30,7 +30,7 @@ namespace Users
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Users", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Products", Version = "v1" });
             });
         }
 
@@ -41,7 +41,7 @@ namespace Users
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Users v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Products v1"));
             }
 
             app.UseHttpsRedirection();
